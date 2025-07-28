08e0c90a5e817aba09562d3ac0ee5c1efc4a0c25
Genma-shi <123936882+Genma-shi@users.noreply.github.com>
2025-07-28 18:00:13 +0600

    Create storage.py

---
+++ core/storage.py
@@ -0,0 +1,5 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class PublicMediaStorage(S3Boto3Storage):
+    default_acl = 'public-read'  # Делает каждый файл публичным
+    file_overwrite = False       
